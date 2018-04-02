6c458d451eeb09099ba39cc3e11914c6be7261c8
André Gomes <andgomes95@gmail.com>
2018-04-02 10:24:49 -0300

    make makefile

--- src/modules/createCanvas.cpp
+++ src/modules/createCanvas.cpp
@@ -1,8 +1,4 @@
-#include <GL/glut.h>
-#include <GL/gl.h>  
-#include <GL/glu.h> 
-#include <string.h>
-#include "include/createCanvas.h"
+#include "createCanvas.h"
 
 mosaicgraph_window_t * mosaicgraph_create_window(float width, float height){
 	mosaicgraph_window_t * window = (mosaicgraph_window_t *) malloc(sizeof(mosaicgraph_window_t));
@@ -25,14 +21,3 @@ int mosaicgraph_draw_window(mosaicgraph_window_t * window){
 	glutSwapBuffers(); 
     return window->id;
 }
-
-/*int main (int argc, char** argv){
-	glutInit(&argc, argv);// Initialize GLUT
-
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
-	mosaicgraph_window_t * window1 = mosaicgraph_create_window(200,400);
-	strcpy(window1->title, "Hey Ho, let's go!");
-    mosaicgraph_draw_window(window1);
-    glutMainLoop();           
-	return 0;
-}*/
---
+++ src/modules/house.cpp
@@ -0,0 +1,12 @@
+#include "createCanvas.h"
+
+int main (int argc, char** argv){
+	glutInit(&argc, argv);
+
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH);  
+	mosaicgraph_window_t * window1 = mosaicgraph_create_window(200,400);
+	strcpy(window1->title, "Hey Ho, let's go!");
+    mosaicgraph_draw_window(window1);
+    glutMainLoop();           
+	return 0;
+}
